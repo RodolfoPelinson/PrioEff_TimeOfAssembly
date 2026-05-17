ae011655c31a705317e05016064af152be88b599
Rodolfo Pelinson <rodolfopelinson@gmail.com>
2026-05-17 16:00:10 +0100

    Adding analysis of Hobo data

--- functions/letters.R
+++ functions/letters.R
@@ -1,7 +1,7 @@
-letters <- function(x, y, label, cex = 2, font = 2){
+letters <- function(x, y, label, cex = 2, font = 2, adj = c(0,1)){
   oldmar <- par()
   par(new = TRUE, mar = c(0,0,0,0))
   plot(NA, xlim = c(0,100), ylim = c(0,100), xlab = "", ylab = "", xaxs = "i", yaxs = "i", bty = "n", xaxt = "n", yaxt = "n")
-  text(label, x = x, y = y, cex = cex, font = font)
+  text(label, x = x, y = y, cex = cex, font = font, adj = adj)
   par(new = oldmar$new, mar = oldmar$mar)
 }
